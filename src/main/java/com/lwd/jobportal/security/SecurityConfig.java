da3fea72b90b07e57b2ecccb21dbf6e64053187b
Chetan Purkar <157473249+Chetan-Purkar@users.noreply.github.com>
2026-04-14 17:10:06 +0530

    Add jobseeker cross origin add new app

--- src/main/java/com/lwd/jobportal/security/SecurityConfig.java
+++ src/main/java/com/lwd/jobportal/security/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "https://lwd-jobseeker.netlify.app"
+            "https://lwd-jobseekers.netlify.app"
         ));
 
         configuration.setAllowedMethods(List.of(
